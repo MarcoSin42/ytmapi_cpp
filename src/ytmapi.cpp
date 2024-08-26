32af7e936ea7a825b364a305f63c50ecef46b0dd
Marco S <marcosin918@gmail.com>
2024-08-26 10:17:06 -0600

    fix: Added exception handling to constructor

--- src/ytmapi.cpp
+++ src/ytmapi.cpp
@@ -1,4 +1,6 @@
+#include <exception>
 #include <format>
+#include <stdexcept>
 #include <string>
 #include <fstream>
 
@@ -8,21 +10,30 @@
 
 #include "ytmapi.hpp"
 
+#include "cpr/api.h"
+#include "cpr/bearer.h"
 #include "cpr/body.h"
 #include "cpr/cprtypes.h"
+#include "cpr/parameters.h"
 #include "cpr/response.h"
 
 using std::string, std::format;
 using json = nlohmann::json;
 
 YTMusicBase::YTMusicBase(string oauth_path, string lang) {
-    std::ifstream oauth_file(oauth_path);
+    try {
+        std::ifstream oauth_file(oauth_path);
+    } catch (std::ifstream::failure const&) {
+        throw std::runtime_error("Unable to open the oauth json file");
+    }
 
-
-    json oath_json = json::parse(oauth_file);
-    
-    m_oauthToken = oath_json["access_token"];
-    m_refreshToken = oath_json["refresh_token"];
+    try {
+        json oath_json = json::parse(oauth_path);
+        m_oauthToken = oath_json.at("access_token");
+        m_refreshToken = oath_json.at("refresh_token");
+    } catch (std::exception const&) {
+        throw std::runtime_error("The OAUTH JSON file is incorrectly formatted");
+    }
 
     m_language = lang;
 }
@@ -30,17 +41,18 @@ YTMusicBase::YTMusicBase(string oauth_path, string lang) {
 
 Playlists YTMusicBase::getPlaylists() {
     Playlists output;
-    cpr::Response r = cpr::Post(cpr::Url{"https://music.youtube.com/youtubei/v1/guide?prettyPrint=true"},
-                    cpr::Bearer{m_oauthToken},
-                    cpr::Header{
-                        {"accept", "*/*"},
-                        {"accept-language", "en-US"}, // TODO: Allow for different languages later
-                        {"Authorization", format("Bearer {}", m_oauthToken)}, // TODO: Allow for other types of authentication
-                        {"content-type", "application/json"},
-                        {"priority", "u=1, i"}
-                    },
-                    cpr::Body{R"~({"context":{"client":{"hl":"en","gl":"CA","userAgent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36,gzip(gfe)","clientName":"WEB_REMIX","clientVersion":"1.20240819.01.00"}}})~"}
-                    );
+    cpr::Response r = cpr::Post(
+        cpr::Url{"https://music.youtube.com/youtubei/v1/guide?prettyPrint=true"},
+        cpr::Bearer{m_oauthToken},
+        cpr::Header{
+            {"accept", "*/*"},
+            {"accept-language", "en-US"}, // TODO: Allow for different languages later
+            {"Authorization", format("Bearer {}", m_oauthToken)}, // TODO: Allow for other types of authentication
+            {"content-type", "application/json"},
+            {"priority", "u=1, i"}
+        },
+        cpr::Body{R"~({"context":{"client":{"hl":"en","gl":"CA","userAgent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36,gzip(gfe)","clientName":"WEB_REMIX","clientVersion":"1.20240819.01.00"}}})~"}
+    );
 
     json r_json = json::parse(r.text)["items"][1]["guideSectionRenderer"]["items"];
 
@@ -53,3 +65,26 @@ Playlists YTMusicBase::getPlaylists() {
 
     return output;
 }
+
+
+Tracks YTMusicBase::getPlaylistTracks(string playlistID) {
+    Tracks output;
+    output.reserve(50);
+
+    cpr::Response r = cpr::Get(
+        cpr::Url{"https://youtube.googleapis.com/youtube/v3/playlistItems?"},
+        cpr::Bearer{m_oauthToken},
+        cpr::Header{{"Accept", "application/json"}},
+        cpr::Parameters{
+            {"part", "contentDetails"},
+            {"part", "snippet"},
+            {"maxResults", "50"},
+            {"playlistId", playlistID}
+        }
+    );
+
+
+    return output;
+}
+
+
